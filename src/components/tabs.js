94417335276ad094db542cc0935dd756e5e45fd7
Steven Rosa <s.rosa21@icloud.com>
2021-10-29 19:30:51 -0400

    Completed Tasks 1-4

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +15,18 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+
+  const topicsDiv = document.createElement('div')
+  topicsDiv.classList.add('topics')
+
+  topics.forEach(topic => {
+    const tabDiv = document.createElement('div')
+    tabDiv.classList.add('tab')
+    tabDiv.textContent = topic
+    topicsDiv.appendChild(tabDiv)
+  })
+
+  return topicsDiv
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +37,12 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get('http://localhost:5000/api/topics')
+  .then(resp => {
+    document.querySelector(selector).appendChild(Tabs(resp.data.topics))
+  })
+  .catch(err => console.log(err))
+
 }
 
 export { Tabs, tabsAppender }
